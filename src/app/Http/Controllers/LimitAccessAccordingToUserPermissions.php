bd26b332d72011ed8b279c22df9f00b40a866c8b
Hoang Manh Tien <tienhm@eway.vn>
2022-02-10 18:18:19 +0700

    allow role admin - so becareful about naming role

--- src/app/Http/Controllers/LimitAccessAccordingToUserPermissions.php
+++ src/app/Http/Controllers/LimitAccessAccordingToUserPermissions.php
@@ -7,7 +7,7 @@ trait LimitAccessAccordingToUserPermissions {
         $user = backpack_user();
         $permission = strtolower($this->crud->entity_name).'.'.$this->crud->getCurrentOperation();
         
-        if (! $user->can($permission) && $user->id != 1) {
+        if (! $user->can($permission) && $user->id != 1 && !$user->hasRole("admin")) {
             $this->crud->denyAccess($this->crud->getCurrentOperation());
         }
     }
