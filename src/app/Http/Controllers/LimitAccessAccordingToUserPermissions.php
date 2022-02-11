4db206cf5622f617c8f0bd3f16bad57b0b2c7f74
Hoang Manh Tien <tienhm@eway.vn>
2022-02-11 15:53:55 +0700

    4.2: add config for root id and root role

--- src/app/Http/Controllers/LimitAccessAccordingToUserPermissions.php
+++ src/app/Http/Controllers/LimitAccessAccordingToUserPermissions.php
@@ -6,8 +6,11 @@ trait LimitAccessAccordingToUserPermissions {
     protected function denyAccessIfNoPermission() {
         $user = backpack_user();
         $permission = strtolower($this->crud->entity_name).'.'.$this->crud->getCurrentOperation();
+
+        $default_root_id = config('backpack.permissionmanager.default_root_id');
+        $defautl_root_role = config('backpack.permissionmanager.default_root_role');
         
-        if (! $user->can($permission) && $user->id != 1  && !$user->hasRole("admin")) {
+        if (! $user->can($permission) && $user->id != $default_root_id  && !$user->hasRole($defautl_root_role)) {
             $this->crud->denyAccess($this->crud->getCurrentOperation());
         }
     }
