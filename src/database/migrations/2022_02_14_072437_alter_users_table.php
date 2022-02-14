db65a9d0cd90b7120d714c2c1eee8815c50521af
Hoang Manh Tien <tienhm@eway.vn>
2022-02-14 15:48:32 +0700

    chore: add method to force to change password

---
+++ src/database/migrations/2022_02_14_072437_alter_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->integer('forced_change_pass')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropColumn('forced_change_pass');
+        });    
+    }
+}
